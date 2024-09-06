6f90efe93d161159ab2744becdc867a7b8733251
sktrinh12 <sktrinh12@gmail.com>
2024-09-06 12:41:58 -0500

    filter panel component added

---
+++ src/FilterPanel.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Slider,
+  Typography,
+  Grid,
+} from "@mui/material";
+import { FormSortOrder, FormSortBy } from "./Forms";
+
+type FilterPanelProps = {
+  brand: string;
+  sortOrder: string;
+  priceRange: number[];
+  yearRange: number[];
+  handleSortBrand: (event: SelectChangeEvent<string>) => void;
+  handleSortOrderChange: (event: SelectChangeEvent<string>) => void;
+  handlePriceChange: (event: Event, newValue: number | number[]) => void;
+  handleYearChange: (event: Event, newValue: number | number[]) => void;
+};
+
+const FilterPanel: React.FC<FilterPanelProps> = ({
+  brand,
+  sortOrder,
+  priceRange,
+  yearRange,
+  handleSortOrderChange,
+  handleSortBrand,
+  handlePriceChange,
+  handleYearChange,
+}) => {
+  return (
+    <Grid item xs={6} sm={4} md={3}>
+      <Typography gutterBottom>Brand</Typography>
+      <FormSortBy brand={brand} handleSortBrand={handleSortBrand} />
+      <Typography gutterBottom>Order</Typography>
+      <FormSortOrder
+        sortOrder={sortOrder}
+        handleSortOrderChange={handleSortOrderChange}
+      />
+
+      <Typography gutterBottom>Price Range</Typography>
+      <Slider
+        value={priceRange}
+        onChange={handlePriceChange}
+        valueLabelDisplay="auto"
+        min={0}
+        max={100000}
+        sx={{ marginBottom: 2 }}
+      />
+
+      <Typography gutterBottom>Year Range</Typography>
+      <Slider
+        value={yearRange}
+        onChange={handleYearChange}
+        valueLabelDisplay="auto"
+        min={2000}
+        max={2024}
+      />
+    </Grid>
+  );
+};
+
+export default FilterPanel;
