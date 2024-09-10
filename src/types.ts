e82e90d5879a87ca808eb736e22ba29ff12250da
sktrinh12 <sktrinh12@gmail.com>
2024-09-10 16:34:12 -0500

    change schema based on automate flow

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,15 @@
 export type Car = {
-  _id: string;
-  _source: {
-    brand: string;
-    model: string;
-    price: number;
-    usable_battery: number;
-    real_range: number;
-    efficiency: number;
-    acceleration: number;
-    top_speed: number;
-    year: number;
-    image: string;
-  };
+  id: string;
+  brand: string;
+  model: string;
+  price: number;
+  usable_battery: number;
+  real_range: number;
+  efficiency: number;
+  acceleration: number;
+  top_speed: number;
+  year: number;
+  image: string;
 };
 
 export type CarCardProps = {
