3d72292f99e6125c16df5be91bda58b229b62ba9
sktrinh12 <sktrinh12@gmail.com>
2024-09-11 15:57:34 -0500

    add car context; no need to re-fetch data from car details page

---
+++ src/CarContext.tsx
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useContext } from 'react';
+import { Car } from "./types";
+
+interface CarContextType {
+  carData: Car[] | null;
+  setCarData: React.Dispatch<React.SetStateAction<Car[] | null>>;
+}
+
+const CarContext = createContext<CarContextType | undefined>(undefined);
+
+export const CarProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [carData, setCarData] = useState<Car[] | null>(null);
+
+  return (
+    <CarContext.Provider value={{ carData, setCarData }}>
+      {children}
+    </CarContext.Provider>
+  );
+};
+
+export const useCarContext = () => {
+  const context = useContext(CarContext);
+  if (context === undefined) {
+    throw new Error('useCarContext must be used within a CarProvider');
+  }
+  return context;
+};
