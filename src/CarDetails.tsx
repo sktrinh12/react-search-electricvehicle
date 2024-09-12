ef7b6b5f7aee88f8465f3ea1ce9337eeb23107e9
sktrinh12 <sktrinh12@gmail.com>
2024-09-12 14:25:43 -0500

    use object entries to generate car info grid programmatically, use of context for back button, add price and brand in sep grid

--- src/CarDetails.tsx
+++ src/CarDetails.tsx
@@ -3,20 +3,20 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { Grid, Box, IconButton, Typography } from "@mui/material";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import ContactSeller from "./Contact";
+import { contactData } from "./data";
 import { AZURE_BLOB_SAS_URL } from "./urls";
-
-//import ContactSeller from "./ContactSeller"; // Import contact seller component
 
 const CarDetails = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const car = location.state?.car;
 
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
   const [imageArray, setImageArray] = useState<string[]>([]);
 
   const handleBack = () => {
-    navigate("/"); // Navigate back to the main page
+    navigate("/");
   };
 
   useEffect(() => {
@@ -39,114 +39,178 @@ const CarDetails = () => {
   return (
     <Box
       sx={{
-        boxShadow: 24,
-        p: 2,
         overflow: "auto",
         textAlign: "center",
       }}
     >
-      <Grid container spacing={2}>
-        <Button onClick={handleBack} variant="contained" color="primary">
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          padding: 2,
+          justifyContent: "center",
+        }}
+      >
+        <Button
+          onClick={handleBack}
+          sx={{
+            textTransform: "none", // Disable uppercase text
+            paddingX: 4,
+            paddingY: 1.5,
+            marginTop: 2,
+            marginBottom: 3,
+          }}
+          variant="outlined"
+          color="primary"
+        >
           Back to Cars
         </Button>
       </Grid>
-      <Grid>
-        <Typography variant="h4" gutterBottom>
-          {car.brand} {car.model}
-        </Typography>
-        <IconButton
-          onClick={handlePrev}
-          sx={{
-            position: "absolute",
-            left: 0,
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}
-          disabled={currentImageIndex === 0}
-        >
-          <ArrowBack />
-        </IconButton>
-        {imageArray.length > 0 && (
-          <img
-            src={AZURE_BLOB_SAS_URL(imageArray[currentImageIndex])}
-            alt={`${currentImageIndex + 1} of ${car.brand} ${car.model}`}
-            style={{
-              maxWidth: "100%",
-              maxHeight: "100%",
-              objectPosition: "center",
+
+      <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+        <Grid item xs={12} md={8} sx={{ position: "relative" }}>
+          <Box
+            sx={{
+              position: "relative",
+              width: "600px",
+              height: "400px",
+              margin: "auto",
+              overflow: "hidden",
             }}
-          />
-        )}
-        <IconButton
-          onClick={handleNext}
-          sx={{
-            position: "absolute",
-            right: 0,
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}
-          disabled={currentImageIndex === imageArray.length - 1}
-        >
-          <ArrowForward />
-        </IconButton>
+          >
+            <IconButton
+              onClick={handlePrev}
+              sx={{
+                position: "absolute",
+                left: 10,
+                top: "50%",
+                transform: "translateY(-50%)",
+                zIndex: 1,
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                color: "#fff",
+              }}
+              disabled={currentImageIndex === 0}
+            >
+              <ArrowBack />
+            </IconButton>
+            {imageArray.length > 0 && (
+              <img
+                src={AZURE_BLOB_SAS_URL(imageArray[currentImageIndex])}
+                alt={`${currentImageIndex + 1} of ${car.brand} ${car.model}`}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  objectPosition: "center",
+                }}
+              />
+            )}
+            <IconButton
+              onClick={handleNext}
+              sx={{
+                position: "absolute",
+                right: 10,
+                top: "50%",
+                transform: "translateY(-50%)",
+                zIndex: 1,
+                backgroundColor: "rgba(0, 0, 0, 0.5)", // Slight background for visibility
+                color: "#fff",
+              }}
+              disabled={currentImageIndex === imageArray.length - 1}
+            >
+              <ArrowForward />
+            </IconButton>
+          </Box>
+        </Grid>
+
+        {/* Price Box */}
+        <Grid item xs={12} md={4}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "400px",
+              padding: 2,
+            }}
+          >
+            <Box
+              sx={{
+                backgroundColor: "#fff",
+                padding: 2,
+                borderRadius: "8px",
+                boxShadow: 3,
+                textAlign: "center",
+                width: "100%",
+                maxWidth: "300px",
+              }}
+            >
+              <Typography
+                variant="h4"
+                sx={{
+                  color: "#000",
+                  fontWeight: "bold",
+                  marginBottom: 1,
+                }}
+              >
+                {car.brand} {car.model}
+              </Typography>
+              <Typography
+                variant="h4"
+                sx={{
+                  color: "#555",
+                }}
+              >
+                ${car.price}
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
       </Grid>
 
       {/* Display comprehensive car data */}
       <Grid
-        sx={{ flexGrow: 1, justifyContent: "center", alignItems: "center" }}
+        sx={{
+          flexGrow: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
         container
         spacing={2}
       >
-        <Grid item xs={6}>
-          <Grid container spacing={2}>
-            <Grid item xs={6} className="cell">
-              <Typography>Year</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>{car.year}</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>Price</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>${car.price}</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>Usable Battery (kWh)</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>{car.usable_battery}</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>Real Range (km)</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>{car.real_range}</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>Efficiency (Wh/km)</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>{car.efficiency}</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>Acceleration (sec)</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>{car.acceleration}</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>Top Speed (km/h)</Typography>
-            </Grid>
-            <Grid item xs={6} className="cell">
-              <Typography>{car.top_speed}</Typography>
+        <Grid
+          container
+          spacing={2}
+          sx={{ flexGrow: 1, justifyContent: "center", marginTop: 3 }}
+        >
+          <Grid item xs={12} md={6}>
+            <Grid container spacing={2}>
+              {Object.entries({
+                Year: car.year,
+                "Usable Battery (kWh)": car.usable_battery,
+                "Real Range (km)": car.real_range,
+                "Efficiency (Wh/km)": car.efficiency,
+                "Acceleration (sec)": car.acceleration,
+                "Top Speed (km/h)": car.top_speed,
+              }).map(([label, value]) => (
+                <React.Fragment key={label}>
+                  <Grid item xs={6} className="cell">
+                    <Typography>
+                      <strong>{label}</strong>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6} className="cell">
+                    <Typography>{value}</Typography>
+                  </Grid>
+                </React.Fragment>
+              ))}
             </Grid>
           </Grid>
         </Grid>
       </Grid>
 
       {/* Contact Seller Component */}
-      {/* <ContactSeller seller={car.seller} /> */}
+      <ContactSeller contacts={contactData} />
     </Box>
   );
 };
