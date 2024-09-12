8c428c388092897ef07566d2911fe3ac5ebba808
sktrinh12 <sktrinh12@gmail.com>
2024-09-12 14:24:28 -0500

    pass currentPage for back button logic

--- src/CarContext.tsx
+++ src/CarContext.tsx
@@ -4,15 +4,18 @@ import { Car } from "./types";
 interface CarContextType {
   carData: Car[] | null;
   setCarData: React.Dispatch<React.SetStateAction<Car[] | null>>;
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const CarContext = createContext<CarContextType | undefined>(undefined);
 
 export const CarProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [carData, setCarData] = useState<Car[] | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   return (
-    <CarContext.Provider value={{ carData, setCarData }}>
+    <CarContext.Provider value={{ carData, setCarData, currentPage, setCurrentPage }}>
       {children}
     </CarContext.Provider>
   );
