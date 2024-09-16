d6b154865a41bccc28f887d772913bf6140683cb
sktrinh12 <sktrinh12@gmail.com>
2024-09-16 11:33:48 -0500

    remove box for each item for better alignment

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -4,6 +4,8 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import EmailIcon from "@mui/icons-material/Email";
 
+const COLOUR = "#f57b18";
+
 type ContactInfo = {
   title: string;
   address?: string;
@@ -17,42 +19,36 @@ interface ContactSellerProps {
 
 const ContactSeller: React.FC<ContactSellerProps> = ({ contacts }) => {
   return (
-    <Box padding={3} bgcolor="#f5f5f5">
+    <Box padding={3}>
       <Typography variant="h4" gutterBottom>
         Contact
       </Typography>
-      <Grid container spacing={3} justifyContent="center">
+      <Grid container spacing={2} justifyContent="center">
         {/* Address */}
         <Grid item xs={12} md={4}>
-          <Box display="flex" alignItems="center" mb={2}>
-            <LocationOnIcon style={{ color: "#23288C", marginRight: 8 }} />
-            <Typography variant="body1">
-              <strong>{contacts.title}:</strong>{" "}
-              {contacts.address ? contacts.address : "None"}
-            </Typography>
-          </Box>
+          <LocationOnIcon style={{ color: COLOUR, marginRight: 8 }} />
+          <Typography variant="body1">
+            <strong>{contacts.title}:</strong>{" "}
+            {contacts.address ? contacts.address : "None"}
+          </Typography>
         </Grid>
 
         {/* Phone */}
         <Grid item xs={12} md={4}>
-          <Box display="flex" alignItems="center" mb={2}>
-            <PhoneIcon style={{ color: "#23288C", marginRight: 8 }} />
-            <Typography variant="body1">
-              <strong>Phone:</strong>{" "}
-              {contacts.phone ? contacts.phone : "000-000-0000"}
-            </Typography>
-          </Box>
+          <PhoneIcon style={{ color: COLOUR, marginRight: 8 }} />
+          <Typography variant="body1">
+            <strong>Phone:</strong>{" "}
+            {contacts.phone ? contacts.phone : "000-000-0000"}
+          </Typography>
         </Grid>
 
         {/* Email */}
         <Grid item xs={12} md={4}>
-          <Box display="flex" alignItems="center">
-            <EmailIcon style={{ color: "#23288C", marginRight: 8 }} />
-            <Typography variant="body1">
-              <strong>Email:</strong>{" "}
-              {contacts.email ? contacts.email : "none@mail.com"}
-            </Typography>
-          </Box>
+          <EmailIcon style={{ color: COLOUR, marginRight: 8 }} />
+          <Typography variant="body1">
+            <strong>Email:</strong>{" "}
+            {contacts.email ? contacts.email : "none@mail.com"}
+          </Typography>
         </Grid>
       </Grid>
     </Box>
