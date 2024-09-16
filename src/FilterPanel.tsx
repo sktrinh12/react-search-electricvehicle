60d98ba0ef199160e11d4c2639f40b6ec596a2fd
sktrinh12 <sktrinh12@gmail.com>
2024-09-16 11:34:41 -0500

    add padding for rhs, change select menuitem colours

--- src/FilterPanel.tsx
+++ src/FilterPanel.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  SelectChangeEvent,
-  Slider,
-  Typography,
-  Grid,
-} from "@mui/material";
+import { SelectChangeEvent, Slider, Typography, Grid } from "@mui/material";
 import { FormSortOrder, FormSortBy } from "./Forms";
 
 type FilterPanelProps = {
@@ -31,9 +26,13 @@ const FilterPanel: React.FC<FilterPanelProps> = ({
   handleYearChange,
 }) => {
   return (
-    <Grid item xs={6} sm={4} md={3}>
+    <Grid item xs={6} sm={4} md={3} sx={{ paddingRight: "2px" }}>
       <Typography gutterBottom>Brand</Typography>
-      <FormSortBy brand={brand} carBrands={carBrands} handleSortBrand={handleSortBrand} />
+      <FormSortBy
+        brand={brand}
+        carBrands={carBrands}
+        handleSortBrand={handleSortBrand}
+      />
       <Typography gutterBottom>Order</Typography>
       <FormSortOrder
         sortOrder={sortOrder}
@@ -47,7 +46,24 @@ const FilterPanel: React.FC<FilterPanelProps> = ({
         valueLabelDisplay="auto"
         min={0}
         max={100000}
-        sx={{ marginBottom: 2 }}
+        sx={{
+          marginBottom: 2,
+          marginLeft: "6px",
+          color: "#f57b18", // Set the color of the slider
+          "& .MuiSlider-thumb": {
+            backgroundColor: "#f57b18", // Thumb (the circle) color
+            "&:hover, &.Mui-focusVisible": {
+              boxShadow: "0px 0px 0px 8px rgba(245, 123, 24, 0.16)", // Orange glow on hover
+              backgroundColor: "#f57b18", // Maintain the orange color on hover
+            },
+          },
+          "& .MuiSlider-track": {
+            backgroundColor: "#f57b18", // Track (filled portion) color
+          },
+          "& .MuiSlider-rail": {
+            backgroundColor: "#ddd", // Rail (unfilled portion) color
+          },
+        }}
       />
 
       <Typography gutterBottom>Year Range</Typography>
@@ -57,6 +73,23 @@ const FilterPanel: React.FC<FilterPanelProps> = ({
         valueLabelDisplay="auto"
         min={2000}
         max={2024}
+        sx={{
+          marginLeft: "6px",
+          color: "#f57b18", // Set the color of the slider
+          "& .MuiSlider-thumb": {
+            backgroundColor: "#f57b18",
+            "&:hover, &.Mui-focusVisible": {
+              boxShadow: "0px 0px 0px 8px rgba(245, 123, 24, 0.16)",
+              backgroundColor: "#f57b18",
+            },
+          },
+          "& .MuiSlider-track": {
+            backgroundColor: "#f57b18",
+          },
+          "& .MuiSlider-rail": {
+            backgroundColor: "#ddd",
+          },
+        }}
       />
     </Grid>
   );
