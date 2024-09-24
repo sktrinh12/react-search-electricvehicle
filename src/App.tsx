dcb1a46da17b9ec961ca4e4c87ee1b7b3ecf4722
sktrinh12 <sktrinh12@gmail.com>
2024-09-24 14:50:02 -0500

    shopping cart route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Search from "./Search";
 import CarDetails from "./CarDetails";
+import Cart from './ShoppingCartPage';
 import { Routes, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Search />} />
-        <Route path="/car-details/:id" element={<CarDetails />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Search />} />
+      <Route path="/car-details/:id" element={<CarDetails />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
   );
 };
 
